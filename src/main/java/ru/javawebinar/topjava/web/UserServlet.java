471624e8a70890e499117e16d1781efa80dae7d1
Ihor Berda <ihor.berda@gmail.com>
2016-07-07 15:45:59 +0300

    WebApplication context initialized, the list of users has been sent to userList.jsp

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,8 +1,12 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.web.user.AdminRestController;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,16 +15,23 @@ import java.io.IOException;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
-/**
- * User: gkislin
- * Date: 19.08.2014
- */
 public class UserServlet extends HttpServlet {
     private static final Logger LOG = getLogger(UserServlet.class);
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        LOG.debug("redirect to userList");
-        response.sendRedirect("userList.jsp");
+        LOG.debug("getAll");
+        request.setAttribute("userList", adminController.getAll());
+        request.getRequestDispatcher("/userList.jsp").forward(request, response);
+//        response.sendRedirect("userList.jsp");
+    }
+
+    private AdminRestController adminController;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        WebApplicationContext springContext = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
+        adminController = springContext.getBean(AdminRestController.class);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
