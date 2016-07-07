33b5484c5e5219fa04d1452eab52911cece7af85
Ihor Berda <ihor.berda@gmail.com>
2016-07-07 07:51:24 +0300

    Changed FetchType from EAGER to LAZY for set of roles.

--- src/main/java/ru/javawebinar/topjava/model/User.java
+++ src/main/java/ru/javawebinar/topjava/model/User.java
@@ -12,10 +12,6 @@ import java.util.Date;
 import java.util.EnumSet;
 import java.util.Set;
 
-/**
- * User: gkislin
- * Date: 22.08.2014
- */
 @NamedQueries({
         @NamedQuery(name = User.DELETE, query = "DELETE FROM User u WHERE u.id=:id"),
         @NamedQuery(name = User.BY_EMAIL, query = "SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.email=?1"),
@@ -48,7 +44,7 @@ public class User extends NamedEntity {
     @Enumerated(EnumType.STRING)
     @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
     @Column(name = "role")
-    @ElementCollection(fetch = FetchType.EAGER)
+    @ElementCollection(fetch = FetchType.LAZY)
     protected Set<Role> roles;
 
     @Column(name = "calories_per_day", columnDefinition = "default 2000")
