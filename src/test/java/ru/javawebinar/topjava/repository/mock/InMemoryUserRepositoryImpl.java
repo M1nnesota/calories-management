5d85163bca4793edde2368ee2f6ef103d407d76c
Ihor Berda <igberda2011@gmail.com>
2016-07-14 17:10:40 +0300

    Made some refactoring

--- src/test/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
+++ src/test/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
@@ -12,15 +12,10 @@ import javax.annotation.PreDestroy;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
-/**
- * GKislin
- * 15.06.2015.
- */
 @Repository
 public class InMemoryUserRepositoryImpl implements UserRepository {
     private static final Logger LOG = LoggerFactory.getLogger(InMemoryUserRepositoryImpl.class);
